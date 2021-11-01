b753c0aeca58f0cf68f6d56206e4b1dd18a4639d
Fernando <36194099+mffernando@users.noreply.github.com>
2021-11-01 19:37:25 -0300

    search function

---
+++ lib/api/api.dart
@@ -0,0 +1,2 @@
+String token = "your api key here";
+String url = "https://owlbot.info/api/v4/dictionary/";
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:dictionary_app/api/api.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +15,146 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dictionary App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  @override
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  //controllers
+  TextEditingController _controller = TextEditingController();
+  late StreamController _streamController;
+  late Stream _stream;
+  Timer? _debounce;
+
+  //initialize
+  @override
+  void initState() {
+    super.initState();
+
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+
   }
 
-  @override
+  Future _search() async {
+    //if search box empty
+    if(_controller.text == null || _controller.text.length == 0) {
+      _streamController.add(null);
+      return;
+    }
+    _streamController.add("loading");
+    Response response = await get(Uri.parse(url + _controller.text.trim()), headers: {"Authorization": "Token " + token});
+    _streamController.add(json.decode(response.body));
+  }
+  
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: const Text("Dictionary App"),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(48.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24.0),
+                  ),
+                  child: TextFormField(
+                    //search
+                    onChanged: (String text) async {
+                      if(_debounce?.isActive ?? false) _debounce!.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 1000), () {
+                        _search();
+                      });
+                    },
+                    //get user text value
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: "Search for a word",
+                      contentPadding: EdgeInsets.only(left: 24.0),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+
+              IconButton(
+                onPressed: () {
+                  _search();
+                },
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //data from api to stream
+      body: Container(
+        margin: const EdgeInsets.all(8.0),
+        child: StreamBuilder(
+          stream: _stream,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.data == null) {
+              return const Center(
+                child: Text("Enter a search word"),
+              );
+            }
+
+            //loading
+            if(snapshot.data == "loading") {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return ListView.builder(
+                itemCount: snapshot.data["definitions"].length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListBody(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.grey[300],
+                        child: ListTile(
+                          //word image
+                          leading: snapshot.data["definitions"][index]["image_url"] == null ? null : CircleAvatar(
+                            backgroundImage: NetworkImage(snapshot.data["definitions"][index]["image_url"]),
+                          ),
+                          title: Text(_controller.text.trim() + "(" + snapshot.data["definitions"][index]["type"] + ")"),
+                        ),
+                      ),
+                      //definition
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(snapshot.data["definitions"][index]["definition"]),
+                      ),
+                    ],
+                  );
+                }
+            );
+          },
+        ),
+      ),
     );
   }
 }
